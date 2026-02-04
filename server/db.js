45529b903a762ce71dd3b5851331f185636f5859
learningAppProject <shruti.4041@gmail.com>
2026-02-04 09:59:53 +0530

    Prepared backend for deployment

--- server/db.js
+++ server/db.js
@@ -1,17 +1,42 @@
-const mysql= require('mysql2');
-const db=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'shruti5R*',
-    database:'dsa_tracker'
+// const mysql= require('mysql2');
+// const db=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'shruti5R*',
+//     database:'dsa_tracker'
+// });
+
+// db.connect((err)=>{
+//     if(err){
+//         console.error('Error connecting to MySQL:', err);
+//         return;
+//     }
+//     console.log('Connected to MySQL database');
+// });
+
+// module.exports=db;
+
+//TELLS THE CODE TO USE SSL->REQUIRED FOR CLOUD SECURITY
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // This is required for TiDB Cloud connections
+    }
 });
 
-db.connect((err)=>{
-    if(err){
+db.connect((err) => {
+    if (err) {
         console.error('Error connecting to MySQL:', err);
         return;
     }
-    console.log('Connected to MySQL database');
+    console.log('Connected to Cloud Database successfully!');
 });
 
-module.exports=db;
+module.exports = db;
